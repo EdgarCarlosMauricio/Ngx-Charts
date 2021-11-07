5bb949c3f5e8e7f7d143c564528b87f70a919996
Edgar Carlos Mauricio Quintero Romero <equintero@sisvida.co>
2021-11-07 12:59:06 -0500

    Example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Ngx-Charts
-Test  Charts Angular 12
+
+Paso a Paso Solucionando Errores 
+
+ng new chartangular
+cd chartangular
+npm install @swimlane/ngx-charts --save
+npm install @angular/cdk --save
+npm i --save-dev @types/d3-selection
+npm i --save-dev @types/d3-scale
+
+
+Tenemos Un servicio donde generamos data aleatorio al pulsar un boton
+
+En el module importamos 
+NgxChartsModule y BrowserAnimationsModule
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+import { CountryService } from './services/country.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  view: [number, number] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
+
+  constructor(private countryService: CountryService) {}
+
+  get single() {
+    return this.countryService.countryData;
+  }
+  
+  onRandomData() {
+    this.countryService.ramdomdata();
+  }
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
